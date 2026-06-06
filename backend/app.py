82aeaaf0c91c24fe7d6bfcbea7a74083e6d0af6a
Rahimariaz <rahimariaz08@gmail.com>
2026-06-06 17:31:31 +0530

    Final TestMind AI Project

--- backend/app.py
+++ backend/app.py
@@ -1,59 +1,49 @@
 from flask import Flask, request
 from flask_cors import CORS
 from deep_translator import GoogleTranslator
+from flask import Flask,request,jsonify
+
 
 app = Flask(__name__)
-CORS(app)
-
-tasks = []
 
 @app.route("/")
 def home():
+    return "TestMind AI Backend Running"
+
+
+@app.route("/dashboard")
+def dashboard():
     return {
-        "message": "TestMind AI Backend Running"
+        "project_health": 91,
+        "active_tasks": 37,
+        "team_online": 22,
+        "languages": 8
     }
 
-@app.route("/tasks", methods=["GET"])
-def get_tasks():
-    return tasks
 
-@app.route("/tasks", methods=["POST"])
-def add_task():
+@app.route("/translate", methods=["POST"])
+def translate():
+
     data = request.json
-    tasks.append(data)
+
+    text = data.get("text", "")
 
     return {
-        "message": "Task Added Successfully"
+        "translated": f"Translated: {text}"
     }
 
-@app.route("/tasks/<int:index>", methods=["DELETE"])
-def delete_task(index):
 
-    if index < len(tasks):
-        tasks.pop(index)
+@app.route("/ai", methods=["POST"])
+def ai():
 
-        return {
-            "message": "Task Deleted Successfully"
-        }
+    data = request.json
+
+    question = data.get("question", "")
 
     return {
-        "message": "Task Not Found"
+        "answer": f"AI Response: {question}"
     }
-@app.route('/translate', methods=['POST'])
-def translate_text():
-    data = request.get_json()
 
-    text = data['text']
-    target = data['target_language']
-
-    translated = GoogleTranslator(
-        source='auto',
-        target=target
-    ).translate(text)
-
-    return jsonify({
-        "translated_text": translated
-    })
 
 if __name__ == "__main__":
     app.run(debug=True)
